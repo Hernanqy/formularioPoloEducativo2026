988f8c91d0494c3d9cd447e7da934dc10aeee908
Hernanqy <desarrolladoryotti@gmail.com>
2026-02-01 19:13:15 -0300

    14

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,16 +1,14 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "...",
+  apiKey: "AIzaSyDBEC_hoZ7nvTLtoXiiKUKAqzRmaSlXLyY",
+  authDomain: "formularioactividades-9ab58.firebaseapp.com",
+  projectId: "formularioactividades-9ab58",
+  storageBucket: "formularioactividades-9ab58.firebasestorage.app",
+  messagingSenderId: "889779287379",
+  appId: "1:889779287379:web:273a91bc2fb2645feadda6",
+  measurementId: "G-J30SWL4R5J"
 };
-
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-
-enableIndexedDbPersistence(db).catch(() => {});
